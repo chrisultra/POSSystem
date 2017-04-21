cb0ef150490a493f426b9f8d0b78ca82be49c395
chrisultra <chrismerlone@yahoo.com>
2017-04-21 04:42:46 -0400

    Add project files.

---
+++ POSSystem/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace POSSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.catButton1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.catButton3 = new System.Windows.Forms.Button();
+            this.caButton4 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.catButton2 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.adminToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addCategoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(472, 310);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // catButton1
+            // 
+            this.catButton1.Location = new System.Drawing.Point(490, 27);
+            this.catButton1.Name = "catButton1";
+            this.catButton1.Size = new System.Drawing.Size(128, 37);
+            this.catButton1.TabIndex = 1;
+            this.catButton1.Text = "Category 1";
+            this.catButton1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(318, 504);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 66);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Pay";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(930, 343);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(42, 37);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "<";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(978, 343);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(42, 37);
+            this.button5.TabIndex = 5;
+            this.button5.Text = ">";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // catButton3
+            // 
+            this.catButton3.Location = new System.Drawing.Point(758, 27);
+            this.catButton3.Name = "catButton3";
+            this.catButton3.Size = new System.Drawing.Size(128, 37);
+            this.catButton3.TabIndex = 6;
+            this.catButton3.Text = "Category 3";
+            this.catButton3.UseVisualStyleBackColor = true;
+            // 
+            // caButton4
+            // 
+            this.caButton4.Location = new System.Drawing.Point(892, 27);
+            this.caButton4.Name = "caButton4";
+            this.caButton4.Size = new System.Drawing.Size(128, 37);
+            this.caButton4.TabIndex = 7;
+            this.caButton4.Text = "Category 4";
+            this.caButton4.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(490, 70);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(128, 85);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Item 1";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(624, 70);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(128, 85);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Item 2";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(758, 70);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(128, 85);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Item 3";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(892, 70);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(128, 85);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "Item 4";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // catButton2
+            // 
+            this.catButton2.Location = new System.Drawing.Point(624, 27);
+            this.catButton2.Name = "catButton2";
+            this.catButton2.Size = new System.Drawing.Size(128, 37);
+            this.catButton2.TabIndex = 2;
+            this.catButton2.Text = "Category 2";
+            this.catButton2.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(490, 161);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(128, 85);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "Item 5";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(624, 161);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(128, 85);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "Item 6";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(758, 161);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(128, 85);
+            this.button14.TabIndex = 14;
+            this.button14.Text = "Item 7";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(892, 161);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(128, 85);
+            this.button15.TabIndex = 15;
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(490, 252);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(128, 85);
+            this.button16.TabIndex = 16;
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(624, 252);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(128, 85);
+            this.button17.TabIndex = 17;
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(758, 252);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(128, 85);
+            this.button18.TabIndex = 18;
+            this.button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(892, 252);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(128, 85);
+            this.button19.TabIndex = 19;
+            this.button19.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1029, 24);
+            this.menuStrip1.TabIndex = 20;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adminToolStripMenuItem,
+            this.addCategoryToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(61, 20);
+            this.toolStripMenuItem1.Text = "Settings";
+            // 
+            // adminToolStripMenuItem
+            // 
+            this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.adminToolStripMenuItem.Text = "Add Item";
+            // 
+            // addCategoryToolStripMenuItem
+            // 
+            this.addCategoryToolStripMenuItem.Name = "addCategoryToolStripMenuItem";
+            this.addCategoryToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.addCategoryToolStripMenuItem.Text = "Add Category";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(978, 530);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 40);
+            this.button1.TabIndex = 21;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 582);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.caButton4);
+            this.Controls.Add(this.catButton3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.catButton2);
+            this.Controls.Add(this.catButton1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "EZPZ POS";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button catButton1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button catButton3;
+        private System.Windows.Forms.Button caButton4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button catButton2;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addCategoryToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
