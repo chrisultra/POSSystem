771c006233ccbf143effce8b0306ebfefaa2c137
chrisultra <chrismerlone@yahoo.com>
2017-05-01 21:52:01 -0400

    Created qauntity keypad

--- POSSystem/Quantity.Designer.cs
+++ POSSystem/Quantity.Designer.cs
@@ -28,20 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.digitButton9 = new System.Windows.Forms.Button();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.digitButton6 = new System.Windows.Forms.Button();
+            this.digitButton3 = new System.Windows.Forms.Button();
+            this.digitButton8 = new System.Windows.Forms.Button();
+            this.digitButton7 = new System.Windows.Forms.Button();
+            this.digitButton0 = new System.Windows.Forms.Button();
+            this.digitButton5 = new System.Windows.Forms.Button();
+            this.digitButton2 = new System.Windows.Forms.Button();
+            this.digitButton4 = new System.Windows.Forms.Button();
+            this.digitButton1 = new System.Windows.Forms.Button();
+            this.digitTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // digitButton9
+            // 
+            this.digitButton9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.digitButton9.Location = new System.Drawing.Point(132, 160);
+            this.digitButton9.Name = "digitButton9";
+            this.digitButton9.Size = new System.Drawing.Size(54, 46);
+            this.digitButton9.TabIndex = 0;
+            this.digitButton9.Text = "9";
+            this.digitButton9.UseVisualStyleBackColor = true;
+            // 
+            // enterButton
+            // 
+            this.enterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterButton.Location = new System.Drawing.Point(72, 212);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(114, 46);
+            this.enterButton.TabIndex = 1;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            // 
+            // digitButton6
+            // 
+            this.digitButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.digitButton6.Location = new System.Drawing.Point(132, 108);
+            this.digitButton6.Name = "digitButton6";
+            this.digitButton6.Size = new System.Drawing.Size(54, 46);
+            this.digitButton6.TabIndex = 2;
+            this.digitButton6.Text = "6";
+            this.digitButton6.UseVisualStyleBackColor = true;
+            // 
+            // digitButton3
+            // 
+            this.digitButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.digitButton3.Location = new System.Drawing.Point(132, 56);
+            this.digitButton3.Name = "digitButton3";
+            this.digitButton3.Size = new System.Drawing.Size(54, 46);
+            this.digitButton3.TabIndex = 3;
+            this.digitButton3.Text = "3";
+            this.digitButton3.UseVisualStyleBackColor = true;
+            // 
+            // digitButton8
+            // 
+            this.digitButton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.digitButton8.Location = new System.Drawing.Point(72, 160);
+            this.digitButton8.Name = "digitButton8";
+            this.digitButton8.Size = new System.Drawing.Size(54, 46);
+            this.digitButton8.TabIndex = 4;
+            this.digitButton8.Text = "8";
+            this.digitButton8.UseVisualStyleBackColor = true;
+            // 
+            // digitButton7
+            // 
+            this.digitButton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.digitButton7.Location = new System.Drawing.Point(12, 160);
+            this.digitButton7.Name = "digitButton7";
+            this.digitButton7.Size = new System.Drawing.Size(54, 46);
+            this.digitButton7.TabIndex = 5;
+            this.digitButton7.Text = "7";
+            this.digitButton7.UseVisualStyleBackColor = true;
+            // 
+            // digitButton0
+            // 
+            this.digitButton0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.digitButton0.Location = new System.Drawing.Point(12, 212);
+            this.digitButton0.Name = "digitButton0";
+            this.digitButton0.Size = new System.Drawing.Size(54, 46);
+            this.digitButton0.TabIndex = 6;
+            this.digitButton0.Text = "0";
+            this.digitButton0.UseVisualStyleBackColor = true;
+            // 
+            // digitButton5
+            // 
+            this.digitButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.digitButton5.Location = new System.Drawing.Point(72, 108);
+            this.digitButton5.Name = "digitButton5";
+            this.digitButton5.Size = new System.Drawing.Size(54, 46);
+            this.digitButton5.TabIndex = 7;
+            this.digitButton5.Text = "5";
+            this.digitButton5.UseVisualStyleBackColor = true;
+            // 
+            // digitButton2
+            // 
+            this.digitButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.digitButton2.Location = new System.Drawing.Point(72, 56);
+            this.digitButton2.Name = "digitButton2";
+            this.digitButton2.Size = new System.Drawing.Size(54, 46);
+            this.digitButton2.TabIndex = 8;
+            this.digitButton2.Text = "2";
+            this.digitButton2.UseVisualStyleBackColor = true;
+            // 
+            // digitButton4
+            // 
+            this.digitButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.digitButton4.Location = new System.Drawing.Point(12, 108);
+            this.digitButton4.Name = "digitButton4";
+            this.digitButton4.Size = new System.Drawing.Size(54, 46);
+            this.digitButton4.TabIndex = 9;
+            this.digitButton4.Text = "4";
+            this.digitButton4.UseVisualStyleBackColor = true;
+            // 
+            // digitButton1
+            // 
+            this.digitButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.digitButton1.Location = new System.Drawing.Point(12, 56);
+            this.digitButton1.Name = "digitButton1";
+            this.digitButton1.Size = new System.Drawing.Size(54, 46);
+            this.digitButton1.TabIndex = 10;
+            this.digitButton1.Text = "1";
+            this.digitButton1.UseVisualStyleBackColor = true;
+            // 
+            // digitTextBox
+            // 
+            this.digitTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.digitTextBox.Location = new System.Drawing.Point(12, 12);
+            this.digitTextBox.Name = "digitTextBox";
+            this.digitTextBox.Size = new System.Drawing.Size(173, 29);
+            this.digitTextBox.TabIndex = 11;
+            this.digitTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // Quantity
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(201, 274);
+            this.Controls.Add(this.digitTextBox);
+            this.Controls.Add(this.digitButton1);
+            this.Controls.Add(this.digitButton4);
+            this.Controls.Add(this.digitButton2);
+            this.Controls.Add(this.digitButton5);
+            this.Controls.Add(this.digitButton0);
+            this.Controls.Add(this.digitButton7);
+            this.Controls.Add(this.digitButton8);
+            this.Controls.Add(this.digitButton3);
+            this.Controls.Add(this.digitButton6);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.digitButton9);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Quantity";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quantity";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button digitButton9;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Button digitButton6;
+        private System.Windows.Forms.Button digitButton3;
+        private System.Windows.Forms.Button digitButton8;
+        private System.Windows.Forms.Button digitButton7;
+        private System.Windows.Forms.Button digitButton0;
+        private System.Windows.Forms.Button digitButton5;
+        private System.Windows.Forms.Button digitButton2;
+        private System.Windows.Forms.Button digitButton4;
+        private System.Windows.Forms.Button digitButton1;
+        private System.Windows.Forms.TextBox digitTextBox;
     }
 }
--- POSSystem/Quantity.cs
+++ POSSystem/Quantity.cs
@@ -12,9 +12,47 @@ namespace POSSystem
 {
     public partial class Quantity : Form
     {
+       
+        StringBuilder sb = new StringBuilder();
+
+
         public Quantity()
         {
             InitializeComponent();
+
+            loadEventHandlers();
+
+        }
+
+
+
+
+        public void loadEventHandlers()
+        {
+            // Create new event handlers for category and item buttons
+            digitButton0.Click += new EventHandler(this.digitButtonClick);
+            digitButton1.Click += new EventHandler(this.digitButtonClick);
+            digitButton2.Click += new EventHandler(this.digitButtonClick);
+            digitButton3.Click += new EventHandler(this.digitButtonClick);
+            digitButton4.Click += new EventHandler(this.digitButtonClick);
+            digitButton5.Click += new EventHandler(this.digitButtonClick);
+            digitButton6.Click += new EventHandler(this.digitButtonClick);
+            digitButton7.Click += new EventHandler(this.digitButtonClick);
+            digitButton8.Click += new EventHandler(this.digitButtonClick);
+            digitButton9.Click += new EventHandler(this.digitButtonClick);
+        }
+
+        void digitButtonClick(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+       
+            this.sb.Append(button.Text);
+
+            Console.WriteLine(button.Text);
+            this.digitTextBox.Text = sb.ToString();
+
         }
     }
 }
+
